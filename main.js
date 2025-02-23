cf7ebf863761de6c4c71b42407a4139e960c41aa
pizzaglitch <93340219+pizzaglitch@users.noreply.github.com>
2025-02-23 17:11:37 -0500

    Add files via upload
    Added chart.js to display total ASP ticket window breakdown by 5-minute intervals. Added in-line CSS to center, and add gap to,  html elements

--- main.js
+++ main.js
@@ -20,12 +20,15 @@ let numTicketsOnSameDay = 0;
 let ticketsOnSameDay = [];
 
 //Graph variables
-let ticketTimes = [];
+let ticketTimes = []
 let violationStartTime = 0;
 let violationStartTimeNum = 0;
 let violationEndTime = 0;
 let violationEndTimeNum = 0;
-let violationTimeIntervals = [];
+//let violationTimeIntervals = [];
+//let sortedTimes = new Array(19).fill(null);
+//let xAxisTimes= [];
+
 dailyTicketTimes = {
   'Sunday': [],
   'Monday': [],
@@ -42,6 +45,8 @@ formElement.addEventListener('submit', event => {
     const houseNumber = document.getElementById('houseNumber').value;
     const street = document.getElementById('street').value.toUpperCase();
 
+
+
     getStreetCodes(houseNumber, street);
     resetMenu();
 });
@@ -54,6 +59,7 @@ async function getStreetCodes(houseNumber, street) {
   //This url takes the JSON export from the website and modifies the address with user input. 
   const url2 = "https://data.cityofnewyork.us/resource/pvqr-7yc4.json?$query=SELECT%0A%20%20%60summons_number%60%2C%0A%20%20%60plate_id%60%2C%0A%20%20%60registration_state%60%2C%0A%20%20%60plate_type%60%2C%0A%20%20%60issue_date%60%2C%0A%20%20%60violation_code%60%2C%0A%20%20%60vehicle_body_type%60%2C%0A%20%20%60vehicle_make%60%2C%0A%20%20%60issuing_agency%60%2C%0A%20%20%60street_code1%60%2C%0A%20%20%60street_code2%60%2C%0A%20%20%60street_code3%60%2C%0A%20%20%60vehicle_expiration_date%60%2C%0A%20%20%60violation_location%60%2C%0A%20%20%60violation_precinct%60%2C%0A%20%20%60issuer_precinct%60%2C%0A%20%20%60issuer_code%60%2C%0A%20%20%60issuer_command%60%2C%0A%20%20%60issuer_squad%60%2C%0A%20%20%60violation_time%60%2C%0A%20%20%60time_first_observed%60%2C%0A%20%20%60violation_county%60%2C%0A%20%20%60violation_in_front_of_or_opposite%60%2C%0A%20%20%60house_number%60%2C%0A%20%20%60street_name%60%2C%0A%20%20%60intersecting_street%60%2C%0A%20%20%60date_first_observed%60%2C%0A%20%20%60law_section%60%2C%0A%20%20%60sub_division%60%2C%0A%20%20%60violation_legal_code%60%2C%0A%20%20%60days_parking_in_effect%60%2C%0A%20%20%60from_hours_in_effect%60%2C%0A%20%20%60to_hours_in_effect%60%2C%0A%20%20%60vehicle_color%60%2C%0A%20%20%60unregistered_vehicle%60%2C%0A%20%20%60vehicle_year%60%2C%0A%20%20%60meter_number%60%2C%0A%20%20%60feet_from_curb%60%2C%0A%20%20%60violation_post_code%60%2C%0A%20%20%60violation_description%60%2C%0A%20%20%60no_standing_or_stopping_violation%60%2C%0A%20%20%60hydrant_violation%60%2C%0A%20%20%60double_parking_violation%60%0ASEARCH%20%22" + houseNumber +  "%20" + street + "%22";
   try {
+
     const response = await fetch(url2);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
@@ -63,7 +69,7 @@ async function getStreetCodes(houseNumber, street) {
     streetCode1 = streetCodeLookup[0].street_code1;
     streetCode2 = streetCodeLookup[0].street_code2;
     streetCode3 = streetCodeLookup[0].street_code3;
-
+    
   } catch (error) {
     console.error(error.message);
   }
@@ -74,7 +80,11 @@ async function getStreetCodes(houseNumber, street) {
 async function getData(streetCode1, streetCode2, streetCode3) {
   let urlDate = new Date();
   let urlPrevYear = urlDate.setFullYear(urlDate.getFullYear()-1);
+  let sortedTimes = new Array(19).fill(null);
+  let violationTimeIntervals = [];
 
+  violationStartTime = 0;
+  violationEndTime = 0;
   //Original attempt at url. Created by me, not taken by website's suggested JSON data (same for url in above function)
   const url = "https://data.cityofnewyork.us/resource/pvqr-7yc4.json?" + "street_code1=" + streetCode1 + "&street_code2=" + streetCode2 + "&street_code3=" + streetCode3 + violation;
 
@@ -96,56 +106,55 @@ async function getData(streetCode1, streetCode2, streetCode3) {
     }
     const nearbyTickets = await response.json();  
     console.log(nearbyTickets);
-
+  
     //counts number of tickets in area on the same day, stores each ticket's violation time in ticketsOnSameDay object
     nearbyTickets.forEach(element => {
       let parkingDate = new Date(element.issue_date);
       let parkingDay = parkingDate.getDay();
       let parkingDayOfWeek = daysOfTheWeek[parkingDay];
       
+      
       if (violationStartTime == 0 && violationEndTime == 0) {
-        violationStartTime = element.from_hours_in_effect;
+        violationStartTime = element.from_hours_in_effect; //use parseTimeFunction here
         violationEndTime = element.to_hours_in_effect;
-        violationStartTimeNum = Number(violationStartTime.slice(0,4));
+        violationStartTimeNum = Number(violationStartTime.slice(0,4)); 
         violationEndTimeNum = Number(violationEndTime.slice(0,4));
+        console.log(violationStartTimeNum, violationEndTimeNum);
       } 
-     
+  
       if (currentDay == parkingDay) {
         ticketsOnSameDay[numTicketsOnSameDay] = element.violation_time;
         numTicketsOnSameDay += 1;
       }
       //Slices times for graph variables. Places in ticketTimes array [needs to be dumped at end]
-      let tempTime = element.violation_time.slice(0,2) + ":" + element.violation_time.slice(2,4);
+      let tempTime = element.violation_time.slice(0,2) + ":" + element.violation_time.slice(2,4); //get rid of this and use new parseTime function
 
       //Sorts ticket times by days in object
       dailyTicketTimes[parkingDayOfWeek].push(tempTime);
 
       //Array of tickets for all days of week
-      ticketTimes.push(tempTime);
-      
-      console.log("address: ", element.house_number, element.street_name, "Ticketed on:", parkingDayOfWeek, "between", element.from_hours_in_effect, element.to_hours_in_effect, "at", element.violation_time);
+      ticketTimes.push(element.violation_time);
+      let startTime = parseTime(violationStartTime).getTime();
+      violationTimeIntervals.push((parseTime(element.violation_time) - startTime) / 60000);
+
+      console.log("address: ", element.house_number, element.street_name, "Ticketed on:", parkingDayOfWeek, "between", violationStartTimeNum, violationEndTimeNum, "at", element.violation_time);
       totalTicketsInArea += 1;
     });    
     console.log("total ASP tickets for the year: ", totalTicketsInArea, " total tickets on " + currentDayofWeek + ": ", numTicketsOnSameDay);
     console.log(ticketTimes);
-    console.log(dailyTicketTimes);
 
     generateChart();
+    //ticketTimes.map(time => (parseTime(time) - startTime) / 60000);
+
+    sortTimes(violationTimeIntervals, sortedTimes);
+    generateTimeChart(sortedTimes);
+    console.log(sortedTimes);
+    resetMenu(sortedTimes);
   } catch (error) {
     console.error(error.message);
   }
 }
 
-// Resets all data
-function resetMenu() {
-  totalTicketsInArea = 0;
-  numTicketsOnSameDay = 0;
-  ticketsOnSameDay = {};
-  ticketTimes = [],
-  streetCode1 = '';
-  streetCode2 = '';
-  streetCode3 = '';
-}
 
 //Map generation
 const map = L.map('map').setView([40.694857277000914, -73.94557962772903], 13);
@@ -155,11 +164,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 map.on('click', grabMapAddress)
 //Grab map address on click
 async function grabMapAddress(e) {
-  
+
   let latlng = e.latlng;
   let grabbedAddressURL = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + latlng.lat + "&lon=" + latlng.lng;
   try {
       const grabbedResponse = await fetch(grabbedAddressURL);
+     
       if (!grabbedResponse.ok) {
       throw new Error(`Response status: ${grabbedResponse.status}`);
       }
@@ -168,19 +178,19 @@ async function grabMapAddress(e) {
       let grabbedHouseNumber = grabbedAddressJSON.address.house_number;
       let grabbedStreetName = grabbedAddressJSON.address.road;
       getStreetCodes(grabbedHouseNumber, grabbedStreetName);
-      resetMenu();
+      
   } catch (error) {
       console.error(error.message);
   }
 }
 
 
-// Need to populate chart accordingly
+// This chart displays the distribution of tickets based on weekdays
 function generateChart() {
   const days = Object.keys(dailyTicketTimes).map(x => x);
   const tickTest = Object.values(dailyTicketTimes).map(x => x.length);
   let chartStatus = Chart.getChart("myChart");
-
+  
   if (chartStatus != undefined) {
     chartStatus.destroy();
   }
@@ -192,7 +202,7 @@ function generateChart() {
           labels: days,
           datasets: [
             {
-              label: 'Ticket Distribution',
+              label: 'Ticket Distribution by Day',
               data: tickTest,
             }
           ]
@@ -201,7 +211,142 @@ function generateChart() {
     )
 };
 
+//Hourly Chart, top prio
+function generateTimeChart(sortedTimes) {
+  let timeChart = Chart.getChart('timeChart');
+  const ticketsByInterval = Object.values(sortedTimes).map(x => x.length);
+  //fillTimeAxis();
 
+  if (timeChart != undefined) {
+    timeChart.destroy();
+  }
+  new Chart(
+    document.getElementById('timeChart'),
+    {
+      type: 'line',
+      data: {
+        labels: fillTimeAxis(),
+        datasets: [{
+            label: 'Ticket Distribution within ASP Time Window (All Days)',
+            data: ticketsByInterval,
+          }]
+      }
+    }
+  )
+}
+
+//Parsetime function. Changes violationTime and violationStartTime to Date objects so that I can find the time difference between the two
+function parseTime(t) {
+  let hour = Number(t.slice(0,2));
+  let min = Number(t.slice(2,4));
+  let ampm = t.slice(t.length-1);
+  
+  if (ampm == 'P' && hour != 12) {
+    hour = hour + 12; 
+  }
+  
+  return new Date(`2000-01-01 ${hour}:${min}`);
+}
+
+function sortTimes(violationTimeIntervals, sortedTimes) {
+    //tempIndex = Math.floor(violationTimeIntervals[i] / 5);
+    //sortedTimes[tempIndex].push(violationTimeIntervals[i]);    
+    for (let i = 0; i < violationTimeIntervals.length; i++) {
+      let tempTime = violationTimeIntervals[i];
+      let tempIndexFloor = Math.floor(tempTime / 5);
+      let tempIndexReg = tempTime / 5;
+
+      if (tempTime % 5 == 0 && sortedTimes[tempIndexReg] == null) {
+        sortedTimes[tempIndexReg] = [tempTime];
+      }
+
+      if (sortedTimes[tempIndexFloor] == null) {
+        sortedTimes[tempIndexFloor] = [tempTime];
+      }
+    
+      else if (tempTime % 5 == 0) {
+        sortedTimes[tempIndexReg].splice(0, 0, tempTime);
+      }
+      else {
+        sortedTimes[tempIndexFloor].splice(0, 0, tempTime);
+      }
+    }
+
+    //Hackjob attempt at just making null indexes into empty arrays. Goal is to have their length be zero, so that I can put that in generateTimeChart
+    for (let i = 0; i < sortedTimes.length; i++) {
+      if (sortedTimes[i] == null) {
+        sortedTimes[i] = [];
+      }
+    }
+}
+
+// function clearSortTimes(sortedTimes) {
+//   for (let index = 0; index < sortedTimes.length; index++) {
+//     let element = sortedTimes[index];
+//     if (element !== null) {
+//       console.log(element)
+//     }
+//   }
+// };
+  
+
+//Fill time array for x axis of generateTimeChart
+
+function fillTimeAxis(){
+  let hour = violationStartTime.slice(0,2);
+  let min = violationStartTime.slice(2,4);
+  let options = { timeStyle: 'short', hour12: true};
+  let date = new Date(2000, 1, 1, hour, min);
+  let interval = 5;
+  let testTimes = [];
+  //let time = date.getHours() + ':' + date.getMinutes
+
+  for(let i=0;i<19;i++){
+    let date2 = date.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
+    testTimes.push(date2);
+    date.setMinutes(date.getMinutes() + interval);
+
+ 
+    /*
+    let ampm = hour >= 12 ? 'PM' : 'AM';
+    let mins = String(date.getMinutes()).padStart(2, "0");
+    testTimes.push(date.getHours() + ':' + mins + ampm);
+    date.setMinutes(date.getMinutes() + interval);
+    */
+  }
+  return testTimes;
+}
+
+// Resets all data
+// To-Fix: does not generate new one on click. states "chartStatus not defined"
+function resetMenu(sortedTimes) {
+  totalTicketsInArea = 0;
+  numTicketsOnSameDay = 0;
+  violationStartTime = 0;
+  violationEndTime = 0;
+  ticketsOnSameDay = {};
+  //clearSortTimes(sortedTimes);
+
+  //sortedTimes = [];
+  ticketTimes = [];
+  violationTimeIntervals = [];
+
+  dailyTicketTimes = {
+    'Sunday': [],
+    'Monday': [],
+    'Tuesday': [],
+    'Wednesday': [],
+    'Thursday': [],
+    'Friday': [],
+    'Saturday': [],
+  };
+
+  streetCode1 = '';
+  streetCode2 = '';
+  streetCode3 = '';
+
+  
+}
 
 /************************************************************************************************************************************************/
 /* Questions for prof baci (Bahtzee, like yahtzee)
@@ -282,4 +427,34 @@ Assume that user wants to park right now, and the current time is within the tic
 
 /*
 change "Welcome" on wordpress to NY / LA, if google want to grab it anyway, it can, it'll just grab that info instead of the incorrect Welcome info.
+*/
+
+/*
+Toggle option
+Heatmap red, yellow, green for parking. Red = most tickets, green = safest (given current time)
+Noisy neighborhood - See which neighborhoods are the loudest
+*/
+
+/*
+function parseTime(t) {
+  let hour = Number(t.slice(0,2);
+  let min = Number(t.slice(2,4));
+  let ampm = t.slice(4);
+  
+  if (ampm === 'P' && hour !== 12) {
+    hour = hour + 12; 
+  }
+  
+  return new Date(`2000-01-01 ${hour}:${min}`);
+}
+
+(parseTime(tempTime).getTime() - (new Date ("2000-01-01 12:00").getTime))) / 60000;
+gives the exact minutes from tempTime (the violation time as as tring) minus violationStartTime (as a date)
+
+let startTime = parseTime(element.violation_start_time//json start time).getTime(); Examplke time, actually use violationStartTime instead of 12:00
+let array = ["0115P", "1245P"];
+
+array.map(time => (parseTime(time) - violationStartTime) / 60000)
+
+//Double check the chart for minutes, because if i change the violationStartTime variable, i don't want that change to affect the 
 */
